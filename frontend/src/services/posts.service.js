7a6f70ef7da7a30ff1c8521af1571203b67946ce
PatiphatRattanosot <654259017@webmail.npru.ac.th>
2024-12-20 16:58:59 +0700

    update services

---
+++ frontend/src/services/posts.service.js
@@ -0,0 +1,36 @@
+import api from './api'
+const api_url = import.meta.env.VITE_POSTS_URL
+
+const createPost = async (post) =>{
+ const response = await api.post(`${api_url}/`,post,{
+     headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response;
+}
+
+const getAllPosts = async () => await api.get(`${api_url}/`)
+
+const getPostById = async (id) => await api.get(`${api_url}/${id}`)
+
+const updatePost = async (post,id) => {
+     const response = await api.post(`${api_url}/${id}`,post,{
+     headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response;
+}
+
+const deletePost = async (id) => api.delete(`${api_url}/${id}`)
+
+const PostsServices ={
+    createPost,
+    getAllPosts,
+    getPostById,
+    updatePost,
+    deletePost
+}
+
+export default PostsServices

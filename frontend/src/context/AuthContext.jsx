b24f126990c96f17c5c498ee38974fb5ed57a18b
PatiphatRattanosot <654259017@webmail.npru.ac.th>
2024-12-20 13:58:47 +0700

    useContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState, useContext, createContext, useEffect } from "react";
 import AuthService from "../services/auth.service";
 import { Cookies } from "react-cookie";
@@ -7,42 +8,32 @@ const cookies = new Cookies();
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(getUser);
+  const [user, setUser] = useState(getUser);
 
-    const login = async (user) => {
-        try {
-            const response = await AuthService.login(user.username, user.password);
-            if (response === 200) {
-                setUser(user)
-            }
-        } catch (error) {
-            console.error("Login failed:", error.message);
-        }
-    };
+  const login = async (user) => setUser(user);
 
+  const logout = () => {
+    AuthService.logout();
+    setUser(null);
+  };
 
-    const logout = () => {
-        AuthService.logout();
-        setUser(null);
-    };
+  function getUser() {
+    const savedUser = cookies.get("user") || null;
+    return savedUser;
+  }
 
-    function getUser() {
-        const savedUser = cookies.get("user") || null;
-        return savedUser;
-    }
+  useEffect(() => {
+    cookies.set("user", JSON.stringify(user), {
+      path: "/",
+      expires: new Date(Date.now() + 86400),
+    });
+  }, [user]);
 
-    useEffect(() => {
-        cookies.set("user", JSON.stringify(user), {
-            path: "/",
-            expires: new Date(Date.now() + 86400),
-        });
-    }, [user]);
-
-    return (
-        <AuthContext.Provider value={{ user, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuthContext = () => useContext(AuthContext);
